2f21e52b256c3e2b8747f505368208d53170d856
Matheus Guilarducci <mguilarducci@gmail.com>
2014-03-10 19:08:25 -0300

    mongodb

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ var express = require('express'),
 
 var app = express();
 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
+app.post('/wines', wines.addWine);
+app.put('/wines/:id', wines.updateWine);
+
 
 app.listen(3000);
 console.log('Listening on port 3000...');
